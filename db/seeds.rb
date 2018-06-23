a0581bb5b35c66a2a3ecbd89dc14a3f388d77177
GayatriTirodkar <2908gayatri@gmail.com>
2018-06-24 00:28:11 +0530

    Model creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+=begin
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'department.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  Department.find_or_create_by(department_name: row['department_name'])
+  #dept = Department.new
+  #dept.department_name = row['department_name']
+  #dept.save!
+end
+
+puts "Successfully inserted #{Department.count} records in Departments Table"
+
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'employee_data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  #employee = Employee.new
+  #employee.name = row['name']
+  #employee.age = row['age']
+  #employee.gender = row['gender']
+  #employee.salary = row['salary']
+  #employee.save!
+  Employee.find_or_create_by(name: row['name'], age: row['age'], gender: row['gender'], salary: row['salary'])
+end
+
+puts "Successfully inserted #{Employee.count} records in Employees Table"
+
+=end
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'employee_data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  puts row
+  puts "=="*10
+  dept = Department.find_or_create_by(_id: row['department_id'], department_name: row['department_name'], department_id: row['department_id'])
+  Employee.create([{name: row['name'], age: row['age'], gender: row['gender'], salary: row['salary'], department: dept}])
+end
